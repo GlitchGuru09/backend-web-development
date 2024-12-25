8f42dc3658bd0b463045fb7a6713c53985345ae7
shrey vernekar <glitchguru03@gmail.com>
2024-12-25 19:35:33 +0530

    tried appendFile and rename functions in node.js

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 const fs = require('fs');
 
-fs.writeFile("test.txt","Hi this is shrey learning basics of node.js",function(err){
+// fs.writeFile("test.txt","Hi this is shrey learning basics of node.js",function(err){
+//     if(err){
+//         console.log(err)
+//     }else{
+//         console.log("Done!")
+//     }
+// })
+
+// fs.appendFile("test.txt"," trying appendFile.",function(err){
+//     if(err){
+//         console.log(err)
+//     }else{
+//         console.log("Done!")
+//     }
+// })
+
+fs.rename("test.txt","sample.txt",function(err){
     if(err){
         console.log(err)
     }else{
