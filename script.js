b69c2f361cf244ac6ec2461882e19931a4ea588b
shrey vernekar <glitchguru03@gmail.com>
2024-12-25 19:59:33 +0530

    tried unlink and rmdir

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 
+//to create a file and write any content in the file
 // fs.writeFile("test.txt","Hi this is shrey learning basics of node.js",function(err){
 //     if(err){
 //         console.log(err)
@@ -8,6 +9,7 @@ const fs = require('fs');
 //     }
 // })
 
+//to add further into the same file
 // fs.appendFile("test.txt"," trying appendFile.",function(err){
 //     if(err){
 //         console.log(err)
@@ -16,6 +18,7 @@ const fs = require('fs');
 //     }
 // })
 
+//to rename the existing file 
 // fs.rename("test.txt","sample.txt",function(err){
 //     if(err){
 //         console.log(err)
@@ -24,10 +27,28 @@ const fs = require('fs');
 //     }
 // })
 
-fs.copyFile("sample.txt","./copy/copy.txt",function(err){
+//to copy the existing file in some other folder or outside
+// fs.copyFile("sample.txt","./copy/copy.txt",function(err){
+//     if(err){
+//         console.log(err)
+//     }else{
+//         console.log("Done!")
+//     }
+// })
+
+//to delete any existing file
+// fs.unlink("sample.txt",function(err){
+//     if(err){
+//         console.log(err.message)
+//     }else{
+//         console.log("Done!")
+//     }
+// })
+
+fs.rmdir("./copy",{recursive:true}, function(err){
     if(err){
-        console.log(err)
+        console.log(err.message)
     }else{
-        console.log("Done!")
+        console.log("removed!")
     }
 })
